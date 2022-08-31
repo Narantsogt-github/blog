337e9d4ffbd3cd5e1d6d7c0147d75761b2e7e215
NTS <nts3681@gmail.com>
2022-08-31 10:31:31 +0800

    react bootstrap linked

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Alert from 'react-bootstrap/Alert'
 
 export default function Home() {
   return (
-    <div className={styles.container}>My blog
+    <div className={styles.container}>{
+<>
+  {[
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'light',
+    'dark',
+  ].map((variant) => (
+    <Alert key={variant} variant={variant}>
+      This is a {variant} alert—check it out!
+    </Alert>
+  ))}
+</>
+
+    }
      </div>
   )
 }
